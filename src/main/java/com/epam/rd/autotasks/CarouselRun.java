c23a3ee53400706dad40cb3bccdfc9f9c9e78a7e
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-11 12:20:13 +0300

    carefully with statics!

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,34 @@
 package com.epam.rd.autotasks;
 
 public class CarouselRun {
+    private final DecrementingCarousel carousel;
+    private int currentPosition=0;
+
+    public CarouselRun(DecrementingCarousel carousel) {
+        this.carousel=carousel;
+    }
 
     public int next() {
-       throw new UnsupportedOperationException();
+        if (isFinished())
+
+            return -1;
+        if (currentPosition >= carousel.capacity)
+            currentPosition = 0;
+
+        while (carousel.values[currentPosition]==0){
+            currentPosition++;
+            if (currentPosition >= carousel.capacity)
+                currentPosition = 0;
+        }
+
+        return carousel.values[currentPosition++]--;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        for (int i:carousel.values) {
+            if (i>0)
+                return false;
+        }
+        return true;
     }
-
 }
